a7ebc82a8c98297ac1af1456f7023f0a0862a516
alex <alexandre.fourcat@epitech.eu>
2019-11-12 10:41:12 +0100

    Adding create room to the protocol.

--- Common/includes/Protocol/CreateRoom.hpp
+++ Common/includes/Protocol/CreateRoom.hpp
@@ -2,16 +2,17 @@
 // Created by alex on 11/10/19.
 //
 
-#ifndef CPP_RTYPE_2019_CREATEGAME_HPP
-#define CPP_RTYPE_2019_CREATEGAME_HPP
+#ifndef CPP_RTYPE_2019_CREATEROOM_HPP
+#define CPP_RTYPE_2019_CREATEROOM_HPP
 
 #include "Command.hpp"
 
 class CreateRoom : public Command {
-    explicit CreateGame(void *data);
+public:
+    explicit CreateRoom(void *data);
 
-    string roomName;
+    string m_roomName;
     static const size_t CREATE_GAME_PAYLOAD_SIZE = sizeof(char) * 256;
 };
 
-#endif //CPP_RTYPE_2019_CREATEGAME_HPP
+#endif //CPP_RTYPE_2019_CREATEROOM_HPP
