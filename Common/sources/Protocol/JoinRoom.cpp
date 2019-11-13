b8965fac27ba621995fd747923da9d3eaa1db9c6
alex <alexandre.fourcat@epitech.eu>
2019-11-13 23:21:29 +0100

    :hammer: :zap: Rework CMake and add JoinRoom message.

--- Common/sources/Protocol/JoinRoom.cpp
+++ Common/sources/Protocol/JoinRoom.cpp
@@ -2,4 +2,8 @@
 // Created by alex on 11/12/19.
 //
 
-#include "JoinRoom.hpp"
+#include "includes/Protocol/JoinRoom.hpp"
+JoinRoom::JoinRoom(string roomName)
+    : Message(JOIN_ROOM)
+    , m_roomName(move(roomName))
+{}
