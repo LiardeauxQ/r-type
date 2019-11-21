d27934bf35c6853fe5dc3672591f8766700b7465
alex <alexandre.fourcat@epitech.eu>
2019-11-21 16:14:03 +0100

    :sparkles: Adding MovementSystem.

---
+++ Client/includes/MovementSystem.hpp
@@ -0,0 +1,11 @@
+//
+// Created by alex on 11/21/19.
+//
+
+#ifndef R_TYPE_MOVEMENTSYSTEM_HPP
+#define R_TYPE_MOVEMENTSYSTEM_HPP
+
+class MovementSystem {
+};
+
+#endif //R_TYPE_MOVEMENTSYSTEM_HPP
---
+++ Client/sources/MovementSystem.cpp
@@ -0,0 +1,5 @@
+//
+// Created by alex on 11/21/19.
+//
+
+#include "MovementSystem.hpp"
