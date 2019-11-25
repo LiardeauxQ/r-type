79fad1ea0e2154b610f37c0a7166bc78249ee9cb
Quentin <quentin.liardeaux@epitech.eu>
2019-11-25 13:54:25 +0100

    :sparkles: Create velocity component

---
+++ Experiments/r-type/src/components/velocity.rs
@@ -0,0 +1,21 @@
+use amethyst::{
+    ecs::prelude::{Component, DenseVecStorage},
+};
+
+pub struct Velocity {
+    pub x: f32,
+    pub y: f32,
+}
+
+impl Velocity {
+    pub fn new(x: f32, y: f32) -> Velocity {
+        Velocity {
+            x,
+            y
+        }
+    }
+}
+
+impl Component for Velocity {
+    type Storage = DenseVecStorage<Self>;
+}
