d9fa98d880d440fa5be1ddcbf691ef2e617b328d
Kilian Perrier <kilian.perrier@epitech.eu>
2019-11-25 18:44:28 +0100

    :sparkles: Adding Health, Shield and Level to mod.

--- Experiments/r-type/src/components/mod.rs
+++ Experiments/r-type/src/components/mod.rs
@@ -1,5 +1,11 @@
-mod velocity;
 mod circle;
+mod velocity;
+mod health;
+mod shield;
+mod level;
 
-pub use self::velocity::Velocity;
 pub use self::circle::Circle;
+pub use self::velocity::Velocity;
+pub use self::health::Health;
+pub use self::shield::Shield;
+pub use self::level::Level;
