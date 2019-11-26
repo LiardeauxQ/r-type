dfd9638c6a0ee7fd1470cbb7ad0d7197ea5f5954
alex <alexandre.fourcat@epitech.eu>
2019-11-26 23:39:07 +0100

    :hammer: Rework events, each producer now have it's own queue.

--- ECS/includes/EventHandler.hpp
+++ ECS/includes/EventHandler.hpp
@@ -15,34 +15,27 @@
 
 using namespace std;
 
-class AbstractEventProducer {
+class IEventProducer {
 public:
-    virtual ~AbstractEventProducer() = default;
-    virtual vector<ecs::Event> fetchEvents() = 0;
+    virtual ~IEventProducer() = default;
+    virtual void pollEvents() = 0;
 };
 
 class EventHandler {
 public:
-    explicit EventHandler(shared_ptr<deque<ecs::Event>> events);
+    EventHandler();
     ~EventHandler();
 
     void start();
     void stop();
-    void lock();
-    void unlock();
 
-    void addProducer(unique_ptr<AbstractEventProducer> producer);
-    void addEvents(vector<ecs::Event> events);
-    void addEvent(const ecs::Event& event);
+    void addProducer(unique_ptr<IEventProducer> producer);
 private:
     void run();
 
-    vector<unique_ptr<AbstractEventProducer>> m_producers;
+    bool m_running;
+    vector<unique_ptr<IEventProducer>> m_producers;
     thread m_eventThread;
-    mutex m_mutex;
-    shared_ptr<deque<ecs::Event>> m_events;
-    bool m_isRunning;
-    bool m_isLock;
 };
 
 #endif //R_TYPE_EVENTHANDLER_HPP
