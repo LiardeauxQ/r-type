15e16051459c210b14d2e2ec7976b2a4cbde3a50
alex <alexandre.fourcat@epitech.eu>
2019-11-27 18:58:11 +0100

    :hammer: Rework IEntityComponentStorage.

--- ECS/includes/IEntityComponentStorage.hpp
+++ ECS/includes/IEntityComponentStorage.hpp
@@ -14,29 +14,29 @@
 namespace ecs {
 
 struct ComponentDoesNotComplyWithSchema : public std::exception {
-    const char* what() const throw()
+    [[nodiscard]] const char* what() const noexcept final
     {
         return "Component does not comply with schema";
     }
 };
 
 struct SchemaNameAlreadyExistsException : public std::exception {
-    const char* what() const throw()
+    [[nodiscard]] const char* what() const noexcept final
     {
         return "Schema already exists";
     }
 };
 
 struct SchemaNotFound : public std::exception {
-    const char* what() const throw()
+    [[nodiscard]] const char* what() const noexcept final
     {
         return "Schema was not found";
     }
 };
 
 class IEntityComponentStorage {
-
 public:
+    virtual ~IEntityComponentStorage() = default;
     virtual Vec<Entity> request(Box<EntityRequest> request) = 0;
     virtual void store(const Vec<Entity>& entities) = 0;
 
