4fbd763e89ddd904f605d0be0edb6fc15b775308
Quentin <quentin.liardeaux@epitech.eu>
2019-11-28 23:54:08 +0100

    :recycle: Rework Collisons components, Create CollisionData structure to stock data relative to a collison, update Collider Component to a marker component

--- Experiments/r-type/src/entities/bullet.rs
+++ Experiments/r-type/src/entities/bullet.rs
@@ -1,16 +1,24 @@
 use amethyst::{
     assets::Handle,
+    ecs::{Entities, LazyUpdate, ReadExpect},
     core::transform::Transform,
     renderer::{SpriteRender, SpriteSheet},
-    ecs::{Entities, LazyUpdate, ReadExpect},
 };
 
-use crate::components::{Bullet, Velocity};
+use crate::components::{
+    Bullet,
+    Velocity,
+    Collidee,
+    Collider,
+    Damage,
+    Team,
+};
 
 pub fn spawn_bullet(entities: &Entities,
                     sprite_sheet: Handle<SpriteSheet>,
                     lazy_update: &ReadExpect<LazyUpdate>,
-                    transform: &Transform) {
+                    transform: &Transform,
+                    team: &Team) {
     let sprite_render = SpriteRender {
         sprite_sheet: sprite_sheet,
         sprite_number: 0,
@@ -21,6 +29,8 @@ pub fn spawn_bullet(entities: &Entities,
     lazy_update.insert(bullet_entity, Velocity::new(150.0, 0.0));
     lazy_update.insert(bullet_entity, transform.clone());
     lazy_update.insert(bullet_entity, sprite_render.clone());
-    //.with(Collidee::default())
-    //.with(Collider::default())
+    lazy_update.insert(bullet_entity, Damage::new(30));
+    lazy_update.insert(bullet_entity, Collidee::default());
+    lazy_update.insert(bullet_entity, Collider);
+    lazy_update.insert(bullet_entity, Team::new(team.id))
 }
--- Experiments/r-type/src/entities/player.rs
+++ Experiments/r-type/src/entities/player.rs
@@ -1,15 +1,25 @@
 use amethyst::{core::transform::Transform, prelude::*, renderer::SpriteRender};
 
-use crate::common::{AssetType, SpriteSheetList};
-use crate::components::{Direction, Player, Velocity, AttackSpeed};
+use crate::common::{SpriteSheetList};
+use crate::components::{
+    Direction,
+    Player,
+    Velocity,
+    AttackSpeed,
+    Collidee,
+    Collider,
+    Damage,
+    Health,
+    Shield,
+    Team,
+};
 use crate::states::{HEIGHT, WIDTH};
 
 pub fn initialize_player(world: &mut World) -> Result<(), &'static str> {
     let mut transform_s1 = Transform::default();
-    println!("Init player");
     let player_sheet = world
         .fetch::<SpriteSheetList>()
-        .get(AssetType::Player)
+        .get("player")
         .ok_or("Cannot fetch sprite sheet")?;
     let sprite_render = SpriteRender {
         sprite_sheet: player_sheet.clone(),
@@ -21,11 +31,15 @@ pub fn initialize_player(world: &mut World) -> Result<(), &'static str> {
         .create_entity()
         .with(Player::new(Direction::Right, 10))
         .with(Velocity::new(70.0, 70.0))
-        //.with(Collidee::default())
-        //.with(Collider::default())
         .with(transform_s1)
         .with(sprite_render.clone())
         .with(AttackSpeed::new(1))
+        .with(Collidee::default())
+        .with(Collider)
+        .with(Damage::new(300))
+        .with(Health::new(400, 400))
+        .with(Shield::new(100, 100))
+        .with(Team::new(1))
         .build();
     Ok(())
 }
