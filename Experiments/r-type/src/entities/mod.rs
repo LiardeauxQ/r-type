9ebab3fee39243010fa35d76a6a639c46c15db7e
Kilian Perrier <kilian.perrier@epitech.eu>
2019-11-30 14:22:22 +0100

    :fire: Removing useless button related code/files.

--- Experiments/r-type/src/entities/mod.rs
+++ Experiments/r-type/src/entities/mod.rs
@@ -1,9 +1,7 @@
 mod camera;
 mod player;
 mod bullet;
-mod button;
 
 pub use self::camera::initialize_camera;
 pub use self::player::initialize_player;
 pub use self::bullet::spawn_bullet;
-pub use self::button::initialize_button;
