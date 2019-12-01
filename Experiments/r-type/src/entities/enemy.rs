078d50fe26242aaa1fd24d7ba9fab433f80a6e6e
Quentin <quentin.liardeaux@epitech.eu>
2019-12-01 22:12:27 +0100

    :sparkles: Add AttackPattern to handle movement of entities based on a mathematical function

--- Experiments/r-type/src/entities/enemy.rs
+++ Experiments/r-type/src/entities/enemy.rs
@@ -6,9 +6,12 @@ use amethyst::{
     renderer::{SpriteRender, SpriteSheet},
 };
 
+use rand::random;
+
 use crate::components::{
     Spawner,
     AttackSpeed,
+    AttackPattern,
     Enemy,
     Velocity,
     Collidee,
@@ -18,6 +21,9 @@ use crate::components::{
     Team,
     Direction,
     Movable,
+    TimeAnimationList,
+    SheetPosition,
+    TimeAnimation
 };
 use crate::states::{WIDTH, HEIGHT};
 
@@ -55,12 +61,22 @@ pub fn spawn_enemy(entities: &Entities,
         sprite_sheet: sprite_sheet,
         sprite_number: 0,
     };
+    let patterns: [AttackPattern; 5] = [
+        AttackPattern::new(AttackPattern::compute_on_x, 50.0, 50.0),
+        AttackPattern::new(AttackPattern::compute_cos, 50.0, 50.0),
+        AttackPattern::new(AttackPattern::compute_sin, 50.0, 50.0),
+        AttackPattern::new(AttackPattern::compute_ascending_slope, 50.0, 50.0),
+        AttackPattern::new(AttackPattern::compute_descending_slope, 50.0, 50.0),
+    ];
+    let mut animations = TimeAnimationList::default();
 
+    animations.insert("moving", TimeAnimation::new(SheetPosition::new(0, 7, 0), 1000, true));
+    animations.run("moving");
     let enemy_entity = entities.create();
     lazy_update.insert(enemy_entity, Enemy);
     lazy_update.insert(enemy_entity, Velocity::new(-100.0, 0.0));
     lazy_update.insert(enemy_entity, transform.clone());
-    lazy_update.insert(enemy_entity, sprite_render.clone());
+    lazy_update.insert(enemy_entity, sprite_render);
     lazy_update.insert(enemy_entity, Collidee::default());
     lazy_update.insert(enemy_entity, Collider);
     lazy_update.insert(enemy_entity, Damage::new(300));
@@ -68,4 +84,6 @@ pub fn spawn_enemy(entities: &Entities,
     lazy_update.insert(enemy_entity, Team::new(team.id));
     lazy_update.insert(enemy_entity, Direction::LEFT);
     lazy_update.insert(enemy_entity, Movable{is_movable: true});
+    lazy_update.insert(enemy_entity, animations);
+    lazy_update.insert(enemy_entity, patterns[rand::random::<usize>() % patterns.len()]);
 }
