676cd0543af5060b8343347005a535b804524fcc
Kilian Perrier <kilian.perrier@epitech.eu>
2019-12-01 23:53:40 +0100

    :sparkles: GameState is now linked to PauseState but is still empty.

--- Experiments/r-type/src/states/game_state.rs
+++ Experiments/r-type/src/states/game_state.rs
@@ -1,26 +1,64 @@
 use amethyst::{
-    input::{is_key_down, VirtualKeyCode},
+    ecs::prelude::Entity,
+    input::{is_close_requested, is_key_down},
     prelude::*,
+    assets::Handle,
+    ui::UiPrefab,
+    winit::VirtualKeyCode,
 };
-use crate::states::pause_state::PauseState;
 
-pub struct GameState;
+use crate::states::delete_hierarchy;
+use crate::states::PauseState;
+
+#[derive(Debug)]
+pub struct GameState {
+    root_ui: Entity,
+    pause_ui: Handle<UiPrefab>,
+    paused: bool,
+}
+
+impl GameState {
+    pub fn new(root_ui: Entity, pause_ui: Handle<UiPrefab>) -> GameState {
+        GameState {
+            root_ui: root_ui,
+            pause_ui: pause_ui,
+            paused: false,
+        }
+    }
+}
 
 impl SimpleState for GameState {
     fn on_start(&mut self, data: StateData<'_, GameData<'_, '_>>) {
         println!("Entering GameState");
     }
 
+    fn on_pause(&mut self, data: StateData<'_, GameData<'_, '_>>) {
+        self.paused = true;
+    }
+
+    fn on_resume(&mut self, data: StateData<'_, GameData<'_, '_>>) {
+        self.paused = false;
+    }
+
     fn handle_event(&mut self, data: StateData<'_, GameData<'_, '_>>, event: StateEvent) -> SimpleTrans {
-        if let StateEvent::Window(event) = &event {
-            if is_key_down(&event, VirtualKeyCode::Escape) {
-                return Trans::Push(Box::new(PauseState));
+        match event {
+            StateEvent::Window(event) => {
+                if is_close_requested(&event) {
+                    Trans::Quit
+                } else if is_key_down(&event, VirtualKeyCode::Escape) {
+                    Trans::Push(Box::new(PauseState::new(
+                        data.world.create_entity().with(self.pause_ui.clone()).build(),
+                    )))
+                } else {
+                    Trans::None
+                }
             }
+            _ => Trans::None
         }
-        Trans::None
     }
 
     fn on_stop(&mut self, data: StateData<'_, GameData<'_, '_>>) {
+        delete_hierarchy(self.root_ui, data.world).expect("Failed to remove game state");
         println!("Leaving GameState");
     }
 }
