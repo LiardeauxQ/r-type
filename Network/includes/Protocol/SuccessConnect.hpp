2facaa70aa92f0b990890cf20fb6e990f0affc88
Quentin <quentin.liardeaux@epitech.eu>
2019-12-23 14:12:21 +0100

    Resolve compilation error with SuccessConnect

--- Network/includes/Protocol/SuccessConnect.hpp
+++ Network/includes/Protocol/SuccessConnect.hpp
@@ -5,18 +5,23 @@
 #ifndef R_TYPE_SUCCESSCONNECT_HPP
 #define R_TYPE_SUCCESSCONNECT_HPP
 
+#include <string>
+
+#include "Message.hpp"
+#include "protocol.hpp"
+
 class SuccessConnect : public Message {
 public:
     SuccessConnect(uint8_t id)
             : Message(TCP_COMMUNICATION::SUCCESS_CONNECT)
-            , m_id(id) {}
+            , m_idPlayer(id) {}
     SuccessConnect(void *data);
     ~SuccessConnect() override = default;
 
     [[nodiscard]] std::vector<uint8_t> serialize() const override;
     size_t getSize() const final { return PACKET_HDR_SIZE + SUCCESS_CONNECT_SIZE; }
 
-    uint8_t getId() const { return m_id; }
+    uint8_t getId() const { return m_idPlayer; }
 private:
     uint8_t m_idPlayer;
 };
