d4ae00d33fa94878ab0ab02c298896f96fb08d6c
Kilian Perrier <kilian.perrier@epitech.eu>
2019-12-28 15:21:15 +0100

    :recycle: State's udpHandler switch from std::shared to boost::shared.

--- Client/includes/State.hpp
+++ Client/includes/State.hpp
@@ -29,7 +29,7 @@ class State {
         State(std::shared_ptr<GameData> gameData)
         : m_gameData(std::move(gameData)), m_window(nullptr), m_deltaTime(0), m_isPaused(false) {};
         virtual ~State() {};
-        void linkToGame(std::shared_ptr<UdpCommunication> udpHandler, sf::RenderWindow *window, float *deltaTime)
+        void linkToGame(boost::shared_ptr<UdpCommunication> udpHandler, sf::RenderWindow *window, float *deltaTime)
         { m_udpHandler = std::move(udpHandler); m_window = window; m_deltaTime = deltaTime; };
         virtual void onStart() = 0;
         virtual void onStop() = 0;
@@ -39,7 +39,7 @@ class State {
         virtual Transition handleEvent(sf::Event &event) = 0;
     protected:
         std::shared_ptr<GameData> m_gameData;
-        std::shared_ptr<UdpCommunication> m_udpHandler;
+        boost::shared_ptr<UdpCommunication> m_udpHandler;
         sf::RenderWindow *m_window;
         float *m_deltaTime;
         bool m_isPaused;
